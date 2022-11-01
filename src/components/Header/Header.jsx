da96d52d0a3d80c8ce233114eda7ca040354aef8
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-11-01 11:38:44 +0000

    changes to styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <div className='header'>
         <h1 className='header__title'>HELLO 👋, I'M ANDREW DOHERTY</h1>
-        <h3 className='header__info'>I'm a Junior Software developer from Manchester, UK</h3>
-        <Buttons title={"TAKE A LOOK AT MY PROJECTS"} />
+        <h3 className='header__info'>I'm a Junior Software Developer from Manchester, UK</h3>
+        <a href="#projects"><button className='header__button'> TAKE A LOOK AT MY PROJECTS</button></a>
     </div>
   )
 }
